5aae4a101c96dc31165c1cc4751b6a47afc47c4d
AguriCano <aguricano@gmail.com>
2025-04-15 16:24:49 -0500

    finished the final program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Exercise> exerciseList = new List<Exercise>();
+        
+        Running running = new Running("January 15, 2025", 29, 4.9);
+        exerciseList.Add(running);
+
+        StationaryBicycles stationaryBicycles = new StationaryBicycles("April 18, 2025", 58, 17);
+        exerciseList.Add(stationaryBicycles);
+
+        Swimming swimming = new Swimming("Jun 14, 2025", 21, 23);
+        exerciseList.Add(swimming);
+
+        Console.WriteLine("Exercises:");
+        Console.WriteLine();
+
+        foreach (Exercise exercise in exerciseList) 
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
